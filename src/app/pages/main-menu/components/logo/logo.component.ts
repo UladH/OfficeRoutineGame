0fa7ef68f35e31471ff61f1b80e5958953f8d999
UladH <uladzislau.holub@gmail.com>
2022-05-10 00:36:30 +0400

    Added logo

---
+++ src/app/pages/main-menu/components/logo/logo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'org-logo',
+  templateUrl: './logo.component.html',
+  styleUrls: ['./logo.component.scss']
+})
+export class LogoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/pages/main-menu/main-menu.module.ts
+++ src/app/pages/main-menu/main-menu.module.ts
@@ -4,13 +4,15 @@ import { SharedModule } from 'src/app/shared/shared.module';
 import { I18N_PROVIDER } from './i18n/_i18n.provider';
 import { MainMenuRoutingModule } from './main-menu-routing.module';
 import { TranslocoModule } from '@ngneat/transloco';
+import { LogoComponent } from './components/logo/logo.component';
 
 @NgModule({
   providers:[
     I18N_PROVIDER
   ],
   declarations: [
-    PageComponent
+    PageComponent,
+    LogoComponent
   ],
   imports: [
     MainMenuRoutingModule,
